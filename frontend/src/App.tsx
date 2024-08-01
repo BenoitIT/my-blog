16970c3b7ed9571b716a2a5d88ed3879a019d719
BenoitIT <benoitnda35@gmail.com>
2024-08-01 08:45:22 +0200

    ###Create starter React project, structure files, and install Tailwind CSS - Initialize a new React project using Create React App. - Organize the project into a standardized directory structure. - Add Tailwind CSS to the project for efficient and customizable styling. - Set up a basic app structure with initial components and styles.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center font-bold flex justify-center h-screen w-screen text-3xl">
+      My blog app
+    </div>
+  );
+}
+
+export default App;
