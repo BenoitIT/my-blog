a1f6080a56e5e1ccf000f4c08bea89b32ecdc8be
BenoitIT <benoitnda35@gmail.com>
2024-08-01 19:47:30 +0200

    conflict resolution

--- frontend/src/pages/singlePost.tsx
+++ frontend/src/pages/singlePost.tsx
@@ -5,11 +5,7 @@ import { timestampFormatter } from "../utilities/timeStampFormatter";
 import Input from "../components/Input";
 import { useState } from "react";
 import Button from "../components/button";
-<<<<<<< HEAD
-const queryClient = new QueryClient();
-=======
 import Cookies from "js-cookie";
->>>>>>> 339c907 (api integration on both users, posts and authentication)
 const SingleBlog = () => {
   const params: any = useParams();
   const [comment, setComment] = useState("");
@@ -57,22 +53,6 @@ const SingleBlog = () => {
               Written at: <span>{timestampFormatter(data?.createdAt)}</span>
             </p>
           </div>
-<<<<<<< HEAD
-          <Input
-            type="text"
-            label="Write a comment"
-            change={setComment}
-            value={comment}
-            placeholder="type..."
-          />
-          <div className="w-full">
-            <Button
-              label="Save"
-              extrastyle="border rounded-xl py-2 px-4 ml-3"
-              click={handleSubmit}
-            />
-          </div>
-=======
           {Cookies.get("authToken") ? (
             <>
               <Input
@@ -93,7 +73,6 @@ const SingleBlog = () => {
           ) : (
             ""
           )}
->>>>>>> 339c907 (api integration on both users, posts and authentication)
           <p className="font-medium text-gray-600 ml-3">
             Comments {`(${data.comments.length})`}
           </p>
--- frontend/src/services/posts.ts
+++ frontend/src/services/posts.ts
@@ -1,15 +1,9 @@
 import Cookies from "js-cookie";
-<<<<<<< HEAD
-export const fetchPosts = async (limit: number) => {
-  const response = await fetch(
-    `http://localhost:3000/api/v1/posts?limit=${limit}`
-=======
 const apiUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchPosts = async (limit: number) => {
   const response = await fetch(
     `${apiUrl}/api/v1/posts?limit=${limit}`
->>>>>>> 339c907 (api integration on both users, posts and authentication)
   );
   if (!response.ok) {
     throw new Error("Network response was not ok");
@@ -19,11 +13,7 @@ export const fetchPosts = async (limit: number) => {
 };
 export const fetchAuthUserPosts = async (limit: number) => {
   const response = await fetch(
-<<<<<<< HEAD
-    `http://localhost:3000/api/v1/user/posts?limit=${limit}`,
-=======
     `${apiUrl}/api/v1/user/posts?limit=${limit}`,
->>>>>>> 339c907 (api integration on both users, posts and authentication)
     {
       headers: {
         "Content-Type": "application/json",
@@ -39,11 +29,7 @@ export const fetchAuthUserPosts = async (limit: number) => {
 };
 
 export  const fetchPostById = async (postId: number) => {
-<<<<<<< HEAD
-  const response = await fetch(`http://localhost:3000/api/v1/post/${postId}`);
-=======
   const response = await fetch(`${apiUrl}/api/v1/post/${postId}`);
->>>>>>> 339c907 (api integration on both users, posts and authentication)
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
@@ -57,11 +43,7 @@ interface CommentData {
 }
 
 export const postComment = async (commentData: CommentData) => {
-<<<<<<< HEAD
-  const response = await fetch(`http://localhost:3000/api/v1/post/${commentData.postId}/comment`, {
-=======
   const response = await fetch(`${apiUrl}/api/v1/post/${commentData.postId}/comment`, {
->>>>>>> 339c907 (api integration on both users, posts and authentication)
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -87,11 +69,7 @@ interface PostData {
 }
 
 export const createPost = async (postData: PostData) => {
-<<<<<<< HEAD
-  const response = await fetch('http://localhost:3000/api/v1/posts', {
-=======
   const response = await fetch(`${apiUrl}/api/v1/posts`, {
->>>>>>> 339c907 (api integration on both users, posts and authentication)
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
